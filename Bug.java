90069239119e2efba9e270558cbc66d36df12e58
Danu <naomi.l.touchet@gmail.com>
2016-02-19 18:06:35 -0800

    Initial commit, hooray!

---
+++ Bug.java
@@ -0,0 +1,16 @@
+/**
+ * Bug Creator by Charlie and Naomi
+ */
+public class Bug {
+  
+  public Bug() { 
+    /* YOUR CONSTRUCTOR CODE HERE*/
+  }
+  
+  public static void main(String[] args) { 
+    
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
