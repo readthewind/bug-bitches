222a37ea1a6854c27e8e252201925cf13a9cf1db
Danu <naomi.l.touchet@gmail.com>
2016-02-19 18:18:51 -0800

    Adds comments and a new file for the BugVerse

---
+++ BugVerse.java
@@ -0,0 +1,13 @@
+/**
+ * This is the BugVerse!! By Naomi and Charlie
+ */
+public class BugVerse {
+  
+  
+  public static void main(String[] args) { 
+    
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
